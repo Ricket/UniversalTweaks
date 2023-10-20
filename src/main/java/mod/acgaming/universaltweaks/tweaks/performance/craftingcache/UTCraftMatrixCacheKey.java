fb5057b25c25fe6c2dfe447180a262f910ae6111
Richard Carter <richard@richardcarter.org>
2023-10-20 05:38:47 -0500

    Rewrite crafting cache
    Use java.util.Optional. Use HEAD/RETURN mixins instead of a full replacement.

--- src/main/java/mod/acgaming/universaltweaks/tweaks/performance/craftingcache/UTCraftMatrixCacheKey.java
+++ src/main/java/mod/acgaming/universaltweaks/tweaks/performance/craftingcache/UTCraftMatrixCacheKey.java
@@ -1,26 +1,47 @@
 package mod.acgaming.universaltweaks.tweaks.performance.craftingcache;
 
+import java.util.Arrays;
 import java.util.Objects;
 
 import net.minecraft.init.Items;
 import net.minecraft.inventory.InventoryCrafting;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 
 // Courtesy of EverNife
 public class UTCraftMatrixCacheKey
 {
     final int hashCode;
+    final World world;
+    final Item[] items;
+    final int[] damages;
 
-    public UTCraftMatrixCacheKey(InventoryCrafting craftMatrix)
+    public UTCraftMatrixCacheKey(InventoryCrafting craftMatrix, World worldIn)
     {
-        Object[] objects = new Object[craftMatrix.getSizeInventory() * 2];
-        for (int i = 0; i < craftMatrix.getSizeInventory(); i++)
+        world = worldIn;
+
+        int sizeInventory = craftMatrix.getSizeInventory();
+        items = new Item[sizeInventory];
+        int[] maybeDamages = new int[sizeInventory];
+        boolean nonzeroDamage = false;
+        for (int i = 0; i < sizeInventory; i++)
         {
             ItemStack stack = craftMatrix.getStackInSlot(i);
-            objects[i] = stack.getItem();
-            objects[i + craftMatrix.getSizeInventory()] = objects[i] != Items.AIR ? stack.getItemDamage() : 0;
+            Item item = stack.getItem();
+            items[i] = item;
+            int itemDamage = stack.getItemDamage();
+            maybeDamages[i] = item != Items.AIR ? itemDamage : 0;
+            nonzeroDamage = itemDamage != 0 || nonzeroDamage;
         }
-        hashCode = Objects.hash(objects);
+
+        if (nonzeroDamage) {
+            damages = maybeDamages;
+        } else {
+            damages = null;
+        }
+
+        hashCode = Objects.hash(world, Arrays.hashCode(items), Arrays.hashCode(damages));
     }
 
     @Override
@@ -32,7 +53,9 @@ public class UTCraftMatrixCacheKey
     @Override
     public boolean equals(Object o)
     {
+        if (this == o) return true;
+        if (!(o instanceof UTCraftMatrixCacheKey)) return false;
         UTCraftMatrixCacheKey that = (UTCraftMatrixCacheKey) o;
-        return hashCode == that.hashCode;
+        return Objects.equals(world, that.world) && Arrays.equals(items, that.items) && Arrays.equals(damages, that.damages);
     }
 }
--- src/main/java/mod/acgaming/universaltweaks/tweaks/performance/craftingcache/mixin/UTCraftingManagerMixin.java
+++ src/main/java/mod/acgaming/universaltweaks/tweaks/performance/craftingcache/mixin/UTCraftingManagerMixin.java
@@ -1,7 +1,7 @@
 package mod.acgaming.universaltweaks.tweaks.performance.craftingcache.mixin;
 
-import javax.annotation.Nullable;
-
+import it.unimi.dsi.fastutil.objects.Object2ObjectLinkedOpenHashMap;
+import mod.acgaming.universaltweaks.tweaks.performance.craftingcache.UTCraftMatrixCacheKey;
 import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.CraftingManager;
@@ -11,38 +11,113 @@ import net.minecraft.world.World;
 
 import mod.acgaming.universaltweaks.UniversalTweaks;
 import mod.acgaming.universaltweaks.config.UTConfigGeneral;
-import mod.acgaming.universaltweaks.tweaks.performance.craftingcache.UTCraftingCache;
+import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.common.LoaderState;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-// Courtesy of EverNife
+import java.util.Optional;
+
 @Mixin(CraftingManager.class)
 public class UTCraftingManagerMixin
 {
-    /**
-     * @author EverNife
-     * @reason Crafting cache
-     */
-    @Overwrite
-    @Nullable
-    public static IRecipe findMatchingRecipe(InventoryCrafting craftMatrix, World worldIn)
+    private static final Object2ObjectLinkedOpenHashMap<UTCraftMatrixCacheKey, Optional<IRecipe>> NON_NBT_CRAFT_CACHE = new Object2ObjectLinkedOpenHashMap<>();
+
+    /** Carries the key from before findMatchingRecipe to after; so that we don't compute it twice **/
+    private static UTCraftMatrixCacheKey matrixKey;
+
+    @Inject(method = "findMatchingRecipe", at = @At("HEAD"), cancellable = true)
+    private static void beforeFindMatchingRecipe(InventoryCrafting craftMatrix, World worldIn, CallbackInfoReturnable<IRecipe> cir)
     {
-        if (UTConfigGeneral.DEBUG.utDebugToggle) UniversalTweaks.LOGGER.debug("UTCraftingManager ::: Find matching recipe");
-        return UTCraftingCache.findMatchingRecipe(craftMatrix, worldIn);
+        // In case of an unexpected behavior (e.g. exception thrown during findMatchingRecipe), reset matrixKey to null
+        // every time.
+        matrixKey = null;
+
+        if (!isValid(craftMatrix)) {
+            return;
+        }
+
+        matrixKey = new UTCraftMatrixCacheKey(craftMatrix, worldIn);
+        Optional<IRecipe> optionalContent = NON_NBT_CRAFT_CACHE.getAndMoveToFirst(matrixKey);
+        if (optionalContent != null) {
+            if (UTConfigGeneral.DEBUG.utDebugToggle) UniversalTweaks.LOGGER.debug("UTCraftingManager ::: Find matching recipe - cache hit");
+            matrixKey = null;
+            cir.setReturnValue(optionalContent.orElse(null));
+        } else {
+            if (UTConfigGeneral.DEBUG.utDebugToggle) UniversalTweaks.LOGGER.debug("UTCraftingManager ::: Find matching recipe - cache miss");
+        }
     }
 
-    /**
-     * @author EverNife
-     * @reason Crafting cache
-     */
-    @Overwrite
-    public static NonNullList<ItemStack> getRemainingItems(InventoryCrafting craftMatrix, World worldIn)
+    @Inject(method = "findMatchingRecipe", at = @At("RETURN"))
+    private static void afterFindMatchingRecipe(InventoryCrafting craftMatrix, World worldIn, CallbackInfoReturnable<IRecipe> cir)
     {
-        if (UTConfigGeneral.DEBUG.utDebugToggle) UniversalTweaks.LOGGER.debug("UTCraftingManager ::: Get remaining items");
-        IRecipe iRecipe = UTCraftingCache.findMatchingRecipe(craftMatrix, worldIn);
-        if (iRecipe != null) return iRecipe.getRemainingItems(craftMatrix);
-        NonNullList<ItemStack> nonnulllist = NonNullList.withSize(craftMatrix.getSizeInventory(), ItemStack.EMPTY);
-        for (int i = 0; i < nonnulllist.size(); i++) nonnulllist.set(i, craftMatrix.getStackInSlot(i));
-        return nonnulllist;
+        if (matrixKey == null) {
+            return;
+        }
+
+        if (UTConfigGeneral.DEBUG.utDebugToggle) UniversalTweaks.LOGGER.debug("UTCraftingManager ::: Find matching recipe - loading result into cache");
+        NON_NBT_CRAFT_CACHE.putAndMoveToFirst(matrixKey, Optional.ofNullable(cir.getReturnValue()));
+        matrixKey = null;
+    }
+
+    @Inject(method = "findMatchingResult", at = @At("HEAD"), cancellable = true)
+    private static void beforeFindMatchingResult(InventoryCrafting craftMatrix, World worldIn, CallbackInfoReturnable<ItemStack> cir)
+    {
+        if (!isValid(craftMatrix)) {
+            return;
+        }
+
+        Optional<IRecipe> optionalContent = NON_NBT_CRAFT_CACHE.getAndMoveToFirst(new UTCraftMatrixCacheKey(craftMatrix, worldIn));
+        if (optionalContent != null) {
+            if (UTConfigGeneral.DEBUG.utDebugToggle) UniversalTweaks.LOGGER.debug("UTCraftingManager ::: Find matching result - cache hit");
+            cir.setReturnValue(optionalContent.isPresent() ? optionalContent.get().getCraftingResult(craftMatrix) : ItemStack.EMPTY);
+        } else {
+            if (UTConfigGeneral.DEBUG.utDebugToggle) UniversalTweaks.LOGGER.debug("UTCraftingManager ::: Find matching result - cache miss");
+        }
+    }
+
+    @Inject(method = "getRemainingItems", at = @At("HEAD"), cancellable = true)
+    private static void beforeGetRemainingItems(InventoryCrafting craftMatrix, World worldIn, CallbackInfoReturnable<NonNullList<ItemStack>> cir)
+    {
+        if (!isValid(craftMatrix)) {
+            return;
+        }
+
+        Optional<IRecipe> optionalContent = NON_NBT_CRAFT_CACHE.getAndMoveToFirst(new UTCraftMatrixCacheKey(craftMatrix, worldIn));
+        if (optionalContent != null) {
+            if (UTConfigGeneral.DEBUG.utDebugToggle) UniversalTweaks.LOGGER.debug("UTCraftingManager ::: Get remaining items - cache hit");
+            if (optionalContent.isPresent()) {
+                cir.setReturnValue(optionalContent.get().getRemainingItems(craftMatrix));
+            } else {
+                NonNullList<ItemStack> nonnulllist = NonNullList.withSize(craftMatrix.getSizeInventory(), ItemStack.EMPTY);
+                for (int i = 0; i < nonnulllist.size(); ++i)
+                {
+                    nonnulllist.set(i, craftMatrix.getStackInSlot(i));
+                }
+                cir.setReturnValue(nonnulllist);
+            }
+        } else {
+            if (UTConfigGeneral.DEBUG.utDebugToggle) UniversalTweaks.LOGGER.debug("UTCraftingManager ::: Get remaining items - cache miss");
+        }
+    }
+
+    private static boolean isValid(InventoryCrafting craftMatrix)
+    {
+        if (!Loader.instance().hasReachedState(LoaderState.SERVER_STARTING)) {
+            return false;
+        }
+
+        for (int i = 0; i < craftMatrix.getSizeInventory(); i++)
+        {
+            ItemStack itemStack = craftMatrix.getStackInSlot(i);
+            // Skip NBT items
+            if (itemStack.hasTagCompound()) return false;
+
+            // Skip IC2C's stacked items
+            if (Loader.isModLoaded("ic2-classic-spmod") && itemStack.getCount() > 1) return false;
+        }
+        return true;
     }
 }
